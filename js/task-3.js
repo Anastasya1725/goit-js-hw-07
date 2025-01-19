28b62e9a86e0d268a754a1ba2c2d00ee2af31cc4
Anastasia <anastasiapanasian@gmail.com>
2025-01-19 16:21:39 +0300

    goit-js-hw-07

--- js/task-3.js
+++ js/task-3.js
@@ -2,11 +2,7 @@ const nameInput = document.getElementById('name-input');
 const nameOutput = document.getElementById('name-output');
 
 nameInput.addEventListener('input', function(){
-    const trimmedName = nameInput.ariaValueMax.trim();
+    const trimmedName = nameInput.value.trim();
 
-    if (trimmedName === ''){
-        nameOutput.textContent = 'Anonymous'
-    }else{
-        nameOutput.textContent = trimmedName;
-    }
+    nameOutput.textContent = trimmedName ? trimmedName : 'Anonymous';
 });
